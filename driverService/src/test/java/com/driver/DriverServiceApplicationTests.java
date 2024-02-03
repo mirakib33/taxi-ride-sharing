180abed75e0aeec168a387bb5e436287f632c58f
Mazharul Islam Rakib <mirakib33@gmail.com>
2024-02-03 17:01:47 +0600

    Created 3 services

---
+++ driverService/src/test/java/com/driver/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ passengerService/src/test/java/com/passenger/PassengerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
