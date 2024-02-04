6daba508fe7fa36abd915f1fa56fc08059114538
Mazharul Islam Rakib <mirakib33@gmail.com>
2024-02-05 00:21:22 +0600

    Added passenger and driver entity

---
+++ driverService/src/main/java/com/driver/entity/Driver.java
@@ -0,0 +1,41 @@
+package com.driver.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "driver")
+public class Driver {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "driver_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "vehicle_type", nullable = false)
+    private String vehicleType;
+
+    @Column(name = "driving_license_number", unique = true, nullable = false)
+    private String drivingLicenseNumber;
+
+    @Column(name = "rating")
+    private double rating;
+
+    @Column(name = "availability_status", nullable = false)
+    private boolean available;
+}
---
+++ passengerService/src/main/java/com/passenger/entity/Passenger.java
@@ -0,0 +1,35 @@
+package com.passenger.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "passenger")
+public class Passenger {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "passenger_id", nullable = false)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "phone", unique = true, nullable = false)
+    private String phone;
+
+    @Column(name = "city")
+    private String city;
+
+
+}
